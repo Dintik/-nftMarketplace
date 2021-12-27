41c78b6f2939f147bae192772246789d18bfbb9d
Artem Chaylo <achaylo@visiblemagic.com>
2021-12-27 13:16:36 +0200

    some fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,19 +5,21 @@ function MyApp({ Component, pageProps }) {
     return (
         <div>
             <nav className="border-b p-6">
-                <p className="text-4xl font-bold">Metaverse Marketplace</p>
-                <div className="flex mt-4">
+                <p className="text-4xl font-bold text-center">
+                    Artem NFT Marketplace
+                </p>
+                <div className="flex justify-center mt-4">
                     <Link href="/">
-                        <a className="mr-4 text-pink-500">Home</a>
+                        <a className="mr-4 text-pink-600">Home</a>
                     </Link>
                     <Link href="/create-item">
-                        <a className="mr-6 text-pink-500">Sell Digital Asset</a>
+                        <a className="mr-6 text-pink-600">Sell Digital Asset</a>
                     </Link>
                     <Link href="/my-assets">
-                        <a className="mr-6 text-pink-500">My Digital Assets</a>
+                        <a className="mr-6 text-pink-600">My Digital Assets</a>
                     </Link>
                     <Link href="/creator-dashboard">
-                        <a className="mr-6 text-pink-500">Creator Dashboard</a>
+                        <a className="mr-6 text-pink-600">Creator Dashboard</a>
                     </Link>
                 </div>
             </nav>
