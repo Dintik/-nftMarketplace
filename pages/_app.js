c0cd46e29f324ca03768bea3cb58e17982817481
Artem Chaylo <achaylo@visiblemagic.com>
2021-12-29 22:40:05 +0200

    add walet upd

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,41 @@
 import "../styles/globals.css";
 import Link from "next/link";
+import Web3 from "web3";
+import { useEffect, useState } from "react";
+import Web3Modal from "web3modal";
 
 function MyApp({ Component, pageProps }) {
+    const [balance, setBalance] = useState("...");
+    const [networkId, setNetworkId] = useState(null);
+    const [networkName, setNetworkName] = useState("loading");
+    useEffect(() => {
+        loadNFTs();
+    }, []);
+    async function loadNFTs() {
+        const web3Modal2 = new Web3Modal();
+        const connection2 = await web3Modal2.connect();
+        const provider2 = new Web3(connection2);
+
+        const signer = await provider2.eth.getAccounts();
+        const signerBalance = await provider2.eth.getBalance(signer[0]);
+        const signerBalanceEth = provider2.utils.fromWei(signerBalance);
+        const signerBalanceEthFloor =
+            Math.floor(signerBalanceEth * 10000) / 10000;
+
+        const networkId = await provider2.eth.net.getId();
+        const networkName = await provider2.eth.net.getNetworkType();
+        setNetworkId(networkId);
+        setNetworkName(networkName);
+        setBalance(signerBalanceEthFloor);
+    }
     return (
         <div>
             <nav className="border-b p-6">
                 <p className="text-4xl font-bold text-center">
                     Artem NFT Marketplace
+                </p>
+                <p className="text-4xl font-bold text-center">
+                    {balance} ETH - {networkName}
                 </p>
                 <div className="flex justify-center mt-4">
                     <Link href="/">
@@ -21,9 +50,17 @@ function MyApp({ Component, pageProps }) {
                     <Link href="/creator-dashboard">
                         <a className="mr-6 text-pink-600">Creator Dashboard</a>
                     </Link>
+                    <Link href="/page2">
+                        <a className="mr-6 text-pink-600">Test Page</a>
+                    </Link>
                 </div>
             </nav>
-            <Component {...pageProps} />
+            {networkId === 4 ? (
+                <Component {...pageProps} />
+            ) : (
+                `Your wallet is connected to the ${networkName} network. Please switch to Rinkeby`
+            )}
+
             <p className=" font-bold text-center">v1.1.0</p>
         </div>
     );
